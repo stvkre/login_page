85ae5b28f0153e25fde7ae1198e88890fe3ce303
stvkre <kiariemburuh@gmail.com>
2017-09-24 16:19:50 +0300

    Packages -added importing packages to intergrate with vtl files,import vtl adapter and store information in arrays

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,7 @@
-import static spark.Spark.*;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
